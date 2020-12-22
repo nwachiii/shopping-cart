e538c02089b5d647bb467a4a1130b2699bc660d8
Emmanuel Nwachi <egreatness1@gmail.com>
2020-12-22 16:22:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="App-header">
+        <a href="/">Shopping Cart</a>
+      </header>
+      <main>Product List</main>
+      <footer>All right is reserved</footer>
+    </div>
+  );
+}
+
+export default App;
